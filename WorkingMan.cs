427de7149b41095b436ed2d2529c59589823ef45
Tom Bennett <james.bennett@fireeye.com>
2020-04-04 16:33:09 -0700

    update version number

--- WorkingMan.cs
+++ WorkingMan.cs
@@ -10,7 +10,7 @@ using Oxide.Game.Rust.Cui;
 
 namespace Oxide.Plugins
 {
-    [Info("Working Man", "pilate/mothball187", "0.0.3")]
+    [Info("Working Man", "pilate/mothball187", "0.0.4")]
     [Description("Limit playtime per user")]
 
     class WorkingMan : CovalencePlugin
